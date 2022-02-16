7c5579a9f58a85ccc423de896c363ffb2e037063
RYOTA2528 <1216022@gmail.com>
2022-02-16 21:54:39 +0900

    【Start】EclipseによるJAVAPractice

---
+++ src/JavaPractice/Staffinfo.java
@@ -0,0 +1,32 @@
+package JavaPractice;
+
+//【重要】Stringの後のフィールドは文字列、intの後のフィールドは数字
+// これでStaffというクラスに、名前・ID・メールアドレスを持つ変数の塊を使える
+//【重要】余は、Staffの情報を格納したテンプレート
+class Staff {
+	String name ;
+	int sraffid ;  
+	String email;
+}
+
+
+
+public class Staffinfo {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+//これはyamadaというインスタンス（格納先のこと）
+// スタッフのyamadaと宣言したことでその後new Staffで、『新しくyクラスを生成します』という意味
+		/*
+		 * 	『クラス　変数名　＝　new クラス名（引数）』
+		 */
+		Staff yamada = new Staff();
+// ↓でフィールドに値（nameというフィールドにTaro Yamada）を追加した
+		yamada.name = "Taro Yamada";
+		
+		
+		System.out.println(yamada.name);
+
+	}
+
+}
---
+++ src/learning/java/javasample.java
@@ -0,0 +1,22 @@
+/** Javadock形式
+ * autohor: Ryota Saitor
+ * copyright:https://www.udemy.com/course/10daysjava/learn/lecture/3857292#questions/1904660
+ * 
+ */
+
+
+package learning.java;
+/*
+ * 	Sampleは、"Hello Eclipse"と表示すりプログラムです
+ */
+
+public class javasample {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("Hello Eclipse! ");
+
+		
+	}
+
+}
