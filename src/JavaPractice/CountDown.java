5b7d347a5064cce4464df6ce7c5e7a752370d6b1
RYOTA2528 <1216022@gmail.com>
2022-02-17 16:26:27 +0900

    【ADD】インクリメント・デクリメント演算子とFor文の使い方

---
+++ src/JavaPractice/CountDown.java
@@ -0,0 +1,13 @@
+package JavaPractice;
+
+public class CountDown {
+
+	public static void main(String[] args) {
+	// int iにてiは整数。そしてiの初期値が10。かつiは0より上のまま一つずつ引いていく
+		for(int i = 10; i > 0; --i) {
+			System.out.println(i);
+		}
+
+	}
+
+}
---
+++ src/JavaPractice/CountUp.java
@@ -0,0 +1,18 @@
+package JavaPractice;
+
+public class CountUp {
+
+	public static void main(String[] args) {
+	// インクリメント・デクリメント演算子』・For文
+	  /*　【For文の使い方】
+	   * intにてiは整数。
+	   *int i で整数のi  初期値0から整数iが10よりも小さい間、iを『一つずつ増やしていく』。
+	   * 	 
+	   */
+		for(int i = 0;i <10; i++) {
+			System.out.println(i);
+		}
+
+	}
+
+}
