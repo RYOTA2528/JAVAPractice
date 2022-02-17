7897a1468d0d50a5b95e162bdfec086a9c6253a0
RYOTA2528 <1216022@gmail.com>
2022-02-17 18:15:17 +0900

    【ADD】Scannerクラス・比較演算子を用いたBMI計算結果の算出

--- src/JavaPractice/BmiCalc.java
+++ src/JavaPractice/BmiCalc.java
@@ -1,5 +1,10 @@
 package JavaPractice;
 
+//（総括）標準入力（キーボード入力）からif文のよるBMI計算//
+	//（作業注意点）Scannerによるキーボード入力のため、数値はコメントアウト
+
+import java.util.Scanner;
+
 public class BmiCalc {
 	
 	/* BMI(体格指数)を計算するプログラム
@@ -11,20 +16,38 @@ public class BmiCalc {
 	public static void main(String[] args) {
 		double weight,height,bmi;
 		
-		weight = 71;
+		//weight = 71;
 //		height = 1.8; で行えば下記のまま計算OK　これを代入演算子で考えると
-		height = 180;
+		//height = 180;
+		Scanner stdIn = new Scanner(System.in);
+		//システムストリームに文字列を表示する
+		System.out.println("体重は？(kg):");
+		//double型であったためintではなくnextDoubleというメソッドになる
+		weight = stdIn.nextDouble();
+		//システムストリームに文字列を表示する
+		System.out.println("身長は？(cm):");	
+		//double型であったためintではなくnextDoubleというメソッドになる
+		height = stdIn.nextDouble();
+		
 		height  /= 100;
 		/*height = height / 100 と同じ意味（代入演算子）,
 		 */
 		
 		bmi = weight / (height * height);
 
-				
-		System.out.println(bmi);		
+// BMi指数を出力				
+		System.out.println("あなたのBMIは" + bmi + "です。");	
+// IF文書き方		
+		if (bmi < 18.5) {
+			System.out.println("やせ型です");
+			
+		}else if(bmi < 25) {
+			System.out.println("標準です");  
+		}else {
+			System.out.println("肥満です");
+		}
 		
 	}
-	
 	
 
 }
