c9abbb05c00c016aca23c92bb5a6131f32f89e7f
RYOTA2528 <1216022@gmail.com>
2022-02-19 23:17:39 +0900

    【ADD】配列・インデックスによる操作

---
+++ src/JavaPractice/DisplayMonth2.java
@@ -0,0 +1,16 @@
+package JavaPractice;
+
+public class DisplayMonth2 {
+
+	public static void main(String[] args) {
+		// 始めての配列作成！
+		String[]month = {"January","February","March","April","May","June","July","August","September","Octorbar","November","December"};
+		// 【重要】(int i = 0;i < 12;i++という方法でiは0から12までとなる。++1とforで12まで一つずつ増やす		
+		for (int i = 0;i < 12;i++){
+			System.out.println(month[i]);
+		}
+			
+	}
+	
+}
+
