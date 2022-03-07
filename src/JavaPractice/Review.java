2e402cf9420ccacb824c158e237b97ae2e2b4e74
RYOTA2528 <1216022@gmail.com>
2022-03-07 12:20:14 +0900

    【ADD】自動型変換について

--- src/JavaPractice/Review.java
+++ src/JavaPractice/Review.java
@@ -24,6 +24,34 @@ public class Review {
 		total = ken * tanka; 
 		System.out.println(total);
 		
+		double x=5, y;
+		y = (x + 2) * 3;  //(x + 2.0) * 3.0　のように自動変換機能が設けられる。
+		System.out.println(y);
+		
+		byte byteValue = 120;
+		short shortValue;
+		int intValue;
+		long longValue;
+		
+		shortValue = byteValue;  
+		intValue = shortValue;
+		longValue = intValue;
+		System.out.println(longValue); //byte→short,short→int,int→longのように『大きな型へは自動変換』される。
+		
+		char c = 'あ' ; //char型の文字コードは、16ビットなので正の値をのみでかつ範囲は0～65.535です。この範囲の値をいれることができる整数型は『int』と『long』
+		
+		int n = c;
+		long m = c;
+		
+		double d = c;
+		System.out.println(d);
+		
+		double radius = 12.5;
+		double area =  radius * radius * 3.14;
+		System.out.print("面積＝");  
+		System.out.println(area);   //練習用。円の面積を求める問題。
+		
+		
 	}
 
 }
