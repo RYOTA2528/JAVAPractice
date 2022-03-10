8d3fe313a4defea890c66613ffe2dac1f3820674
RYOTA2528 <1216022@gmail.com>
2022-03-10 14:29:37 +0900

    【ADD】Inputによる計算

--- src/JavaPractice/InputMethod.java
+++ src/JavaPractice/InputMethod.java
@@ -1,12 +1,17 @@
 package JavaPractice;
 
-import lib.Input;  //インポート分
+import lib.Input;  //インポート文
 public class InputMethod {
 	public static void main(String[] args) {
 		// inputクラスの使い方(ex9正方形の面積
-		double x =Input.getDouble();  //１辺の長さを入力して、xに受け取る
+		double x =Input.getDouble();  //１辺の長さを入力して、xに受け取る,getDoubleとすることで、double型の値を入力できる。
 		double s = Math.pow(x, 2);  //xを2乗して面積を計算する
-		System.out.println("正方形の面積" +s);
+		System.out.println("正方形の面積" + s);
+		
+		double y = Input.getDouble();
+		double z = Math.sqrt(y);
+		System.out.println("平方根＝" + z);
+		
 
 	}
 
